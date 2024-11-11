1851b86d69b6eebee4ec3b779a2e89e477eef1c5
Anxcye <axyyu@outlook.com>
2024-11-11 16:15:30 +0800

    pushing changes

--- push.sh
+++ push.sh
@@ -4,5 +4,9 @@ git add .
 git commit -m "pushing changes"
 git push
 
+if [ $1 -eq "" ]; then
+    echo "Please provide a tag version"
+    exit 1
+fi
 git tag -a $1 -m "Releasing version $1"
 git push origin $1
