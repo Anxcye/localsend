32cd70f5e668b7ac2086b275b0d66ee1033fe40d
Anxcye <axyyu@outlook.com>
2024-11-15 14:30:00 +0800

    Release v1.2.1

--- release.sh
+++ release.sh
@@ -4,5 +4,9 @@ VERSION=$( grep '^version:' pubspec.yaml | cut -d ' ' -f 2 | cut -d '+' -f 1 )
 
 echo "Releasing version v$VERSION"
 
+git add .
+git commit -m "Release v$VERSION"
+git push
+
 git tag -a v$VERSION -m "Release v$VERSION"
 git push origin v$VERSION
