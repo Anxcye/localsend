5fc5b7df77ed5c85ea67397968e557d5895fb56d
Anxcye <axyyu@outlook.com>
2024-11-15 15:56:04 +0800

    Release v1.2.13

--- scripts/build_windows.ps1
+++ scripts/build_windows.ps1
@@ -1,40 +1,30 @@
-winget install -e --id JRSoftware.InnoSetup --location "D:\Inno Setup 6" --accept-source-agreements
-# list all installed packages
-winget list
-# list dir d:\inno setup 6
-if (Test-Path "C:\Program Files (x86)\Inno Setup 6") {
-    Write-Output "Inno Setup is installed in C:\Program Files (x86)\Inno Setup 6"
-} else {
-    Write-Output "Inno Setup is not found in the default directory."
-}
+$innoSetupDir = "C:\Program Files (x86)\Inno Setup 6"
 
-ls "D:\Inno Setup 6"
+winget install -e --id JRSoftware.InnoSetup --location $innoSetupDir --accept-source-agreements
 
-Copy-Item "scripts\ChineseSimplified.isl" "D:\Inno Setup 6\Languages\"
+flutter clean
+flutter pub get
+flutter gen-l10n
+flutter build windows
 
-# flutter clean
-# flutter pub get
-# flutter gen-l10n
-# flutter build windows
+Remove-Item "D:\inno" -Force  -Recurse -ErrorAction SilentlyContinue
+New-Item -ItemType Directory -Force -Path "D:\inno"
+Copy-Item -Path "build\windows\x64\runner\Release\*" -Destination "D:\inno" -Recurse
+Copy-Item -Path "windows\runner\resources\app_icon.ico" -Destination "D:\inno\logo.ico"
 
-# Remove-Item "D:\inno" -Force  -Recurse -ErrorAction SilentlyContinue
-# New-Item -ItemType Directory -Force -Path "D:\inno"
-# Copy-Item -Path "build\windows\x64\runner\Release\*" -Destination "D:\inno" -Recurse
-# Copy-Item -Path "windows\runner\resources\app_icon.ico" -Destination "D:\inno\logo.ico"
+Copy-Item -Path "scripts\windows\x64\*" -Destination "D:\inno" -Recurse -ErrorAction SilentlyContinue
+Remove-Item "D:\inno-result" -Force  -Recurse -ErrorAction SilentlyContinue
+New-Item -ItemType Directory -Force -Path "D:\inno-result"
 
-# Copy-Item -Path "scripts\windows\x64\*" -Destination "D:\inno" -Recurse -ErrorAction SilentlyContinue
-# Remove-Item "D:\inno-result" -Force  -Recurse -ErrorAction SilentlyContinue
-# New-Item -ItemType Directory -Force -Path "D:\inno-result"
-
-# # 7z a -tzip "D:\inno-result\app.zip" "D:\inno\*"
+# 7z a -tzip "D:\inno-result\app.zip" "D:\inno\*"
 
 
-# Copy-Item "scripts\ChineseSimplified.isl" "D:\Inno Setup 6\Languages\"
-# Copy-Item "scripts\ChineseTraditional.isl" "D:\Inno Setup 6\Languages\"
-# & "D:\Inno Setup 6\ISCC.exe" ".\scripts\compile_windows_setup-inno.iss"
+Copy-Item "scripts\ChineseSimplified.isl" "$innoSetupDir\Languages\"
+Copy-Item "scripts\ChineseTraditional.isl" "$innoSetupDir\Languages\"
+& "$innoSetupDir\ISCC.exe" ".\scripts\compile_windows_setup-inno.iss"
 
-# Copy-Item "D:\inno-result\app.exe" "build\windows\app.exe"
-# Copy-Item "D:\inno-result\app.zip" "build\windows\app.zip"
+Copy-Item "D:\inno-result\app.exe" "build\windows\app.exe"
+Copy-Item "D:\inno-result\app.zip" "build\windows\app.zip"
 
-# Write-Output 'Generated Windows exe installer!'
+Write-Output 'Generated Windows exe installer!'
 
